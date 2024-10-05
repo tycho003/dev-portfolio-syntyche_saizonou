f708e113c0dca6d965312562cba8187d69c148c6
tycho003 <saizonousyntyche@gmail.com>
2024-10-05 20:50:40 +0100

    Updating...

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,22 +1,15 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
   DiPython,
-  DiGit,
 } from "react-icons/di";
 import {
-  SiRedis,
-  SiFirebase,
   SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import trell from "../../Assets/Projects/trell.png";
+import post from "../../Assets/Projects/post.png";
+import ticket from "../../Assets/Projects/ticket.png";
+import comment from "../../Assets/Projects/comment.png";
+import dashboard from "../../Assets/Projects/dashboard.png";
+import movie from "../../Assets/Projects/movie.png";
+
+
+
 
 function Projects() {
   return (
@@ -23,7 +26,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={trell}
               isBlog={false}
               title="TRELLTECH"
               description="An e-commerce site built with PHP, HTML5 and Tailwindcss"
@@ -33,7 +36,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={post}
               isBlog={false}
               title="POST-IT"
               description="A notes managament app built with Vue js and Tailwind Css."
@@ -43,7 +46,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={ticket}
               isBlog={false}
               title="My ShowTime"
               description="My Show Time is a concert and festival ticket booking platform built using NestJS and *MongoDB. It features both a frontoffice for users to book tickets and a backoffice for administrators to manage concerts and track bookings."
@@ -53,7 +56,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={comment}
               isBlog={false}
               title="COMMENT-IT"
               description="A revolutionary web application that empowers users to comment on any content across the internet . The frontend is written in Vue js. The backend uses the Laravel framework. "
@@ -63,7 +66,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={dashboard}
               isBlog={false}
               title="DASHBOARD"
               description="A web application that works like Netvibes allowing users to subscribe to different services you that are accessible that offer different widgets. This app is built with Django and React js.."
@@ -73,7 +76,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={movie}
               isBlog={false}
               title="Movie joy"
               description="This is a Next.js project bootstrapped with create-next-app. with typescript and tailwind css. My movie joy is a website managed by an admin and a user can comment on his favorite films."
